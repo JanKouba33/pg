fb7819007c221a7b0d5c87242d2abbb921b5577e
JanKouba33 <houbicka@icloud.com>
2024-11-04 13:13:03 +0000

    m

---
+++ cviceni 2-2.py
@@ -0,0 +1,24 @@
+def levensteinova_vzdalenost(dotaz1, dotaz2):
+   
+   i = 0
+   lenght = min(len(dotaz1), len(dotaz2))
+   levenstein = 0
+   while i < lenght:
+       if dotaz1[i] != dotaz2
+    
+        
+    
+
+
+if __name__ == "__main__":
+
+    query1 = "seznam"
+    query2 = "seznamka"
+    query3 = "sesnam"
+    query4 = "seznam"
+
+    print(levensteinova_vzdalenost_for(query1, query2))
+    print(levensteinova_vzdalenost_for(query2, query3))
+    print(levensteinova_vzdalenost_for(query1, query3))
+
+    print(levensteinova_vzdalenost_for(query1, query4))
