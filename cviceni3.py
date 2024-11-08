7cde574b3345915bf75ce6b4f4a8007e9dbb3ba5
JanKouba33 <houbicka@icloud.com>
2024-11-08 08:33:14 +0000

    uloz

--- cviceni3.py
+++ cviceni3.py
@@ -1,11 +1,27 @@
-Ahoj všichni!
-Ahoj ostatní!
+import re
+
+r = re.compile(r"s+")
+
+
+
+def zapis_soubor(jmeno_souboru):
+    fp = open("jmeno_souboru", "w")
+    fp.write("Ahoj                 všichni!\n")
+    fp.write("Ahoj        ostatní!\n")
+    fp.close
+
+def precti_soubor(jmeno_souboru):
+    with open(jmeno_souboru, "r") as fp:
+        data = fp.read()
+        print(data)
+        fp.seek(0)
+        for line in fp:
+            line = line.strip()
+            print(line)
 
 
 
 
 if __name__ == "__main__":
-    fp = open("data.txt", "w")
-    fp.write("Ahoj všichni!\n")
-    fp.write("Ahoj ostatní!\n")
-    fp.close
+    zapis_soubor("data.txt")
+    precti_soubor("data.txt")
