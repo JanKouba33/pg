d0ce1363dc3cfc29c1db8dbb2813efd9825b1c36
JanKouba33 <houbicka@icloud.com>
2024-12-05 09:25:28 +0000

    Hotovo

--- fifth.py
+++ fifth.py
@@ -1,18 +1,20 @@
 import sys
 
-# definice úvodních binárních sekvencí obrázkových souborů
+# Definice úvodních binárních sekvencí obrázkových souborů
 jpeg_header = b'\xff\xd8'
 gif_header1 = b'GIF87a'
 gif_header2 = b'GIF89a'
 png_header = b'\x89PNG\r\n\x1a\n'
 
 
-def read_header(soubor, 2):
+def read_header(file_name, header_length):
     """
     Tato funkce načte binární soubor z cesty file_name,
     z něj přečte prvních header_length bytů a ty vrátí pomocí return
     """
-    return b'xxx'
+    with open(file_name, 'rb') as file:
+        return file.read(header_length)
+        
 
 
 def is_jpeg(file_name):
@@ -20,12 +22,11 @@ def is_jpeg(file_name):
     Funkce zkusí přečíst ze souboru hlavičku obrázku jpeg,
     tu srovná s definovanou hlavičkou v proměnné jpeg_header
     """
-    # načti hlavičku souboru
-    header = read_header(file_name, len(jpeg_header))
-
-    # vyhodnoť zda je soubor jpeg
-
-    return False
+    try:
+        header = read_header(file_name, len(jpeg_header))
+        return header == jpeg_header
+    except:
+        return False
 
 
 def is_gif(file_name):
@@ -33,8 +34,11 @@ def is_gif(file_name):
     Funkce zkusí přečíst ze souboru hlavičku obrázku jpeg,
     tu srovná s definovanými hlavičkami v proměnných gif_header1 a gif_header2
     """
-    # vyhodnoť zda je soubor gif
-    return False
+    try:
+        header = read_header(file_name, max(len(gif_header1), len(gif_header2)))
+        return header.startswith(gif_header1) or header.startswith(gif_header2)
+    except:
+        return False
 
 
 def is_png(file_name):
@@ -42,8 +46,11 @@ def is_png(file_name):
     Funkce zkusí přečíst ze souboru hlavičku obrázku jpeg,
     tu srovná s definovanou hlavičkou v proměnné png_header
     """
-    # vyhodnoť zda je soubor png
-    return False
+    try:
+        header = read_header(file_name, len(png_header))
+        return header == png_header
+    except:
+        return False
 
 
 def print_file_type(file_name):
@@ -61,6 +68,10 @@ def print_file_type(file_name):
 
 
 if __name__ == '__main__':
-    # přidej try-catch blok, odchyť obecnou vyjímku Exception a vypiš ji
-    file_name = sys.argv[1]
-    print_file_type(file_name)
+    try:
+        file_name = sys.argv[1]
+        print_file_type(file_name)
+    except FileNotFoundError:
+        print(f'Soubor {sys.argv[1]} neexistuje.')
+    except Exception:
+        print("Chyba")
--- pokus.py
+++ pokus.py
@@ -1,11 +1,77 @@
-def vrat_treti(seznam):
-    if len(seznam) > 2:
-        return seznam[2]
+import sys
+
+# Definice úvodních binárních sekvencí obrázkových souborů
+jpeg_header = b'\xff\xd8'
+gif_header1 = b'GIF87a'
+gif_header2 = b'GIF89a'
+png_header = b'\x89PNG\r\n\x1a\n'
+
+
+def read_header(file_name, header_length):
+    """
+    Tato funkce načte binární soubor z cesty file_name,
+    z něj přečte prvních header_length bytů a ty vrátí pomocí return
+    """
+    with open(file_name, 'rb') as file:
+        return file.read(header_length)
+        
+
+
+def is_jpeg(file_name):
+    """
+    Funkce zkusí přečíst ze souboru hlavičku obrázku jpeg,
+    tu srovná s definovanou hlavičkou v proměnné jpeg_header
+    """
+    try:
+        header = read_header(file_name, len(jpeg_header))
+        return header == jpeg_header
+    except:
+        return False
+
+
+def is_gif(file_name):
+    """
+    Funkce zkusí přečíst ze souboru hlavičku obrázku jpeg,
+    tu srovná s definovanými hlavičkami v proměnných gif_header1 a gif_header2
+    """
+    try:
+        header = read_header(file_name, max(len(gif_header1), len(gif_header2)))
+        return header.startswith(gif_header1) or header.startswith(gif_header2)
+    except:
+        return False
+
+
+def is_png(file_name):
+    """
+    Funkce zkusí přečíst ze souboru hlavičku obrázku jpeg,
+    tu srovná s definovanou hlavičkou v proměnné png_header
+    """
+    try:
+        header = read_header(file_name, len(png_header))
+        return header == png_header
+    except:
+        return False
+
+
+def print_file_type(file_name):
+    """
+    Funkce vypíše typ souboru - tuto funkci není třeba upravovat
+    """
+    if is_jpeg(file_name):
+        print(f'Soubor {file_name} je typu jpeg')
+    elif is_gif(file_name):
+        print(f'Soubor {file_name} je typu gif')
+    elif is_png(file_name):
+        print(f'Soubor {file_name} je typu png')
     else:
-        return None
+        print(f'Soubor {file_name} je neznámého typu')
 
 
-if __name__ == "__main__":
-    polozky = [1,2,3]
-    vysledek = vrat_treti(polozky)
-    print(vysledek)
+if __name__ == '__main__':
+    try:
+        file_name = sys.argv[1]
+        print_file_type(file_name)
+    except FileNotFoundError:
+        print(f'Soubor {sys.argv[1]} neexistuje.')
+    except Exception:
+        print("Chyba")
