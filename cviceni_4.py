59699ac2ddbd07bffc1b202ab6f8e8b44f7a3b70
JanKouba33 <houbicka@icloud.com>
2024-12-06 15:06:02 +0000

    hh

--- cviceni_4.py
+++ cviceni_4.py
@@ -5,8 +5,10 @@ class Zvire:
 
 class Pes(Zvire) :
     def __init__(self, jmeno, rasa) :
+        super().__init__(jmeno)
         self.rasa = rasa
         print(f"Pes rasy {self.rasa} byl vytvoren")
 
 if __name__ == "__main__":
     pes = Pes("Alik", "jezevcik")
+    pes.jmeno
